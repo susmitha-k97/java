f3732a5b9ba14696d79c6012eef822f6a0c258ce
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:29:31 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+
+public class Demo1 {
+public static void main(String[] args)
+{
+	double d=101.26;
+	int i=99;
+	System.out.println("d="+d);
+	System.out.println("i="+i);
+	i=(int)d;
+	System.out.println("d="+d);
+	System.out.println("i="+i);
+}
+}
