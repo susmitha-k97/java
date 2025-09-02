1812b8c7790bd0d5287dbb646e2c1ac15c96387d
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:47:24 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Example {
+void Helloworld()
+{
+	System.out.println("Hello world");
+}
+	public static void main(String[] args)
+	{
+		Example e1=new Example();
+		Example e2=new Example();
+		e1.Helloworld();
+		e2.Helloworld();
+}
+}
