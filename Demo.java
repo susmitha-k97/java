77a43a4845b2713a1b2bf5ae7838df93aee48435
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:29:02 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+public static void main(String[] args)
+{
+	System.out.println(10);
+	System.out.println(10+20);
+	System.out.println(10+20+" value is ");
+	System.out.println(10+20+" value is "+10);
+	System.out.println(10+20+" value is "+10+20);
+	System.out.println(10+20+" value is "+(10+20));	
+}
+}
