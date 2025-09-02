9d6961058c772a114535471dadf49f698544450b
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:40:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Main {
+static void display() {
+	System.out.println("Java");
+	System.out.println("sql");
+	System.out.println("python");
+}
+public static void main(String[] args)
+{
+	display();
+	display();
+	display();
+}
+}
