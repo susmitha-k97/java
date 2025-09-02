e14dd2792f832289c2c2665a3bf127ce6d12b9a1
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:36:46 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Person {
+String name;
+int age;
+static String nationality="Indian";
+public static void main(String[] args)
+{
+	System.out.println(nationality);
+	System.out.println(Person.nationality);
+	//System.out.println(name);
+}
+}
