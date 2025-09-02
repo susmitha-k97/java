ad01eea37dd535ac2ddea93bb237b94b1c00f629
susmitha-k97 <kummanamalasusmitha@gmail.com>
2025-09-02 16:27:31 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,20 +1,55 @@
-package day1;
+
 public class Student {
-	String StdName="Scott";
-	int RegNum=101;
-	String Branch = "CSE";
-public static void main(String[] args) {
-	Student S1=new Student();
-	System.out.println("Student Name : "+S1.StdName);
-	System.out.println("Register Num : "+S1.RegNum);
-	System.out.println("Branch :"+S1.Branch);
-	Student S2=new Student();
-	System.out.println("Student Name : "+S2.StdName);
-	System.out.println("Register Num : "+S2.RegNum);
-	System.out.println("Branch :"+S2.Branch);
-	Student S3=new Student();
-	System.out.println("Student Name : "+S3.StdName);
-	System.out.println("Register Num : "+S3.RegNum);
-	System.out.println("Branch :"+S3.Branch);
+	String stdname;
+	int regno;
+    double percentage;
+	String branch;
+public static void main(String[] args)
+{
+	Student s1=new Student();
+	s1.stdname="scott";
+	s1.regno=101;
+	s1.percentage=81.2;
+	s1.branch="cse";
+	System.out.println("Student name : "+s1.stdname);
+	System.out.println("Student id: "+s1.regno);
+	System.out.println("Student score: "+s1.percentage);
+	System.out.println("Student branch: "+s1.branch);
+	Student s2=new Student();
+	s2.stdname="tarak";
+	s2.regno=102;
+	s2.percentage=91.2;
+	s2.branch="csm";
+	System.out.println("Student name : "+s2.stdname);
+	System.out.println("Student id: "+s2.regno);
+	System.out.println("Student score: "+s2.percentage);
+	System.out.println("Student branch: "+s2.branch);
+	Student s3=new Student();
+	s3.stdname="yesh";
+	s3.regno=103;
+	s3.percentage=71.2;
+	s3.branch="csd";
+	System.out.println("Student name : "+s3.stdname);
+	System.out.println("Student id: "+s3.regno);
+	System.out.println("Student score: "+s3.percentage);
+	System.out.println("Student branch: "+s3.branch);
+	Student s4=new Student();
+	s4.stdname="sree";
+	s4.regno=104;
+	s4.percentage=51.2;
+	s4.branch="csbs";
+	System.out.println("Student name : "+s4.stdname);
+	System.out.println("Student id: "+s4.regno);
+	System.out.println("Student score: "+s4.percentage);
+	System.out.println("Student branch: "+s4.branch);
+	Student s5=new Student();
+	s5.stdname="somu";
+	s5.regno=105;
+	s5.percentage=79.2;
+	s5.branch="civil";
+	System.out.println("Student name : "+s5.stdname);
+	System.out.println("Student id: "+s5.regno);
+	System.out.println("Student score: "+s5.percentage);
+	System.out.println("Student branch: "+s5.branch);
 }
 }
